eacb80d70a37ff48c8e0fe297854b1ed4a4d0c14
dev4 <gerardm@teknovarik.com>
2019-10-23 15:39:51 +0800

    commit changes

---
+++ kpl_first_design/src/components/globalLayout/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <tool-bar></tool-bar>
+        <v-content app>
+            <v-container>
+                <router-view></router-view>
+            </v-container>
+        </v-content>
+    </div>
+</template>
+<script>
+import toolBar from './toolbar'
+export default {
+    components: {
+        'tool-bar': toolBar
+    }
+}
+</script>
