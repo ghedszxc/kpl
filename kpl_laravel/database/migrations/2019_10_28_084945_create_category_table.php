0f150010a8b7f17cea7c95c0dc7928bbdc987e1c
mardie <mardieg@teknovarik.com>
2019-10-28 16:58:34 +0800

    issue resolved - create table

--- kpl_laravel/database/migrations/2019_10_28_084945_create_category_table.php
+++ kpl_laravel/database/migrations/2019_10_28_084945_create_category_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Category extends Migration
+class CreateCategoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,11 @@ class Category extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('category_name');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +27,6 @@ class Category extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('category');
     }
 }
---
+++ kpl_laravel/database/migrations/2019_10_28_085319_create_brand_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBrandTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brand', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('brand_name');
+            $table->unsignedInteger('category_id');
+            $table->timestamps();
+
+            $table->foreign('category_id')
+            ->references('id')->on('category')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brand');
+    }
+}
