9edee64859c05794a9dd35fce905a80ea44940cb
dev4 <gerardm@teknovarik.com>
2019-10-29 14:26:37 +0800

    commit

--- kpl_first_design/src/components/content/home.vue
+++ kpl_first_design/src/components/content/home.vue
@@ -7,25 +7,25 @@
             <v-flex xs12 sm3 class="px-2 mt-2">
                 <promo-list></promo-list>
             </v-flex>
-            <v-flex xs12 class="mt-2">
+            <!-- <v-flex xs12 class="mt-2">
                 <top-product></top-product>
-            </v-flex>
-            <!-- <v-flex xs12 class="px-2 mt-2">
-                <glimpse-product></glimpse-product>
             </v-flex> -->
+            <v-flex xs12 class="px-2 mt-2">
+                <glimpse-product></glimpse-product>
+            </v-flex>
         </v-layout>
     </div>
 </template>
 <script>
 import featureProduct from './home/featureProduct'
-// import glimpseProuct from './home/glimpseProduct'
-import topProduct from './home/topProduct'
+import glimpseProuct from './home/glimpseProduct'
+// import topProduct from './home/topProduct'
 import promoList from './home/promo'
 export default {
     components: {
         'feature-product': featureProduct,
-        // 'glimpse-product': glimpseProuct,
-        'top-product': topProduct,
+        'glimpse-product': glimpseProuct,
+        // 'top-product': topProduct,
         'promo-list': promoList
     }
 }
--- kpl_first_design/src/components/content/home/glimpseProduct.vue
+++ kpl_first_design/src/components/content/home/glimpseProduct.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-        <v-layout wrap row>
-            <v-flex xs4>
-                <v-menu
+    <div style="border: 1px solid red;">
+        <v-layout wrap row class="ma-1">
+            <v-flex xs2>
+                <!-- <v-menu
                     v-model="menu"
                     :close-on-content-click="false"
                     :nudge-width="200" offset-x>
@@ -16,52 +16,107 @@
                         <v-list>
                         <v-list-item>
                             <v-list-item-avatar>
-                            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+                                <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
                             </v-list-item-avatar>
 
                             <v-list-item-content>
-                            <v-list-item-title>John Leider</v-list-item-title>
-                            <v-list-item-subtitle>Founder of Vuetify.js</v-list-item-subtitle>
+                                <v-list-item-title>John Leider</v-list-item-title>
+                                <v-list-item-subtitle>Founder of Vuetify.js</v-list-item-subtitle>
                             </v-list-item-content>
 
                             <v-list-item-action>
-                            <v-btn :class="fav ? 'red--text' : ''"
-                                icon @click="fav = !fav">
-                                <v-icon>mdi-heart</v-icon>
-                            </v-btn>
+                                <v-btn :class="fav ? 'red--text' : ''"
+                                    icon @click="fav = !fav">
+                                    <v-icon>mdi-heart</v-icon>
+                                </v-btn>
                             </v-list-item-action>
                         </v-list-item>
                         </v-list>
-
-                        <v-divider></v-divider>
-
-                        <v-list>
-                        <v-list-item>
-                            <v-list-item-action>
-                            <v-switch v-model="message" color="purple"></v-switch>
-                            </v-list-item-action>
-                            <v-list-item-title>Enable messages</v-list-item-title>
-                        </v-list-item>
-
-                        <v-list-item>
-                            <v-list-item-action>
-                            <v-switch v-model="hints" color="purple"></v-switch>
-                            </v-list-item-action>
-                            <v-list-item-title>Enable hints</v-list-item-title>
-                        </v-list-item>
-                        </v-list>
-
-                        <v-card-actions>
-                        <v-spacer></v-spacer>
-
-                        <v-btn text @click="menu = false">Cancel</v-btn>
-                        <v-btn color="primary" text @click="menu = false">Save</v-btn>
-                        </v-card-actions>
                     </v-card>
-                </v-menu>
+                </v-menu> -->
+                <v-list dense>
+                    <v-list-item>
+                        <v-list-item-icon> <v-icon>mdi-home</v-icon> </v-list-item-icon>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item>
+            
+                    <v-list-group prepend-icon="account_circle" value="true">
+                        <template v-slot:activator>
+                            <v-list-item-title>Users</v-list-item-title>
+                        </template>
+                
+                        <v-list-group no-action sub-group value="true">
+                            <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-title>Admin</v-list-item-title>
+                            </v-list-item-content>
+                            </template>
+                
+                            <v-list-item v-for="(admin, i) in admins" :key="i" link>
+                            <v-list-item-title v-text="admin[0]"></v-list-item-title>
+                            <v-list-item-icon>
+                                <v-icon v-text="admin[1]"></v-icon>
+                            </v-list-item-icon>
+                            </v-list-item>
+                        </v-list-group>
+                    </v-list-group>
+                </v-list>
             </v-flex>
-            <v-flex xs12 sm8>
-
+            <v-flex xs12 sm10>
+                
+                <v-layout wrap row class="ma-1">
+                    <v-flex xs12 sm4 class="px-1 mt-2">
+                        <v-card>
+                            <v-img class="white--text align-end" height="200px"
+                                src="https://www.bidlink.net/news/wp-content/uploads/2019/08/12_ton_hydraulic_tripod_jack.jpg">
+                            </v-img>
+                            <v-card-text>
+                                <p class="main_font"> Hydraulic Tripod Jack </p>
+                                <span class="font-weight-light">Suspendisse imperdiet, felis eu hendrerit pretium, sapien mauris blandit arcu, et accumsan neque est ut erat. Vivamus eget tellus euismod, interdum nibh at</span>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn text class="light-blue lighten-2 white--text">
+                                    Inquire
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+                    <v-flex xs12 sm4 class="px-1 mt-2">
+                        <v-card>
+                            <v-img class="white--text align-end" height="200px"
+                                src="https://3.imimg.com/data3/AC/LW/MY-7375549/hydax-items-full-range-500x500.jpg">
+                            </v-img>
+                            <v-card-text>
+                                <p class="main_font"> Hydraulic Chuchu </p>
+                                <span class="font-weight-light">Suspendisse imperdiet, felis eu hendrerit pretium, sapien mauris blandit arcu, et accumsan neque est ut erat. Vivamus eget tellus euismod, interdum nibh at</span>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn text class="light-blue lighten-2 white--text">
+                                    Inquire
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+                    <v-flex xs12 sm4 class="px-1 mt-2">
+                        <v-card>
+                            <v-img class="white--text align-end" height="200px"
+                                src="https://dewaldfluidpower.com/wp-content/uploads/2017/10/pro-top-2.jpg">
+                            </v-img>
+                            <v-card-text>
+                                <p class="main_font"> Hydraulic Chuchu </p>
+                                <span class="font-weight-light">Suspendisse imperdiet, felis eu hendrerit pretium, sapien mauris blandit arcu, et accumsan neque est ut erat. Vivamus eget tellus euismod, interdum nibh at</span>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn text class="light-blue lighten-2 white--text">
+                                    Inquire
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
             </v-flex>
         </v-layout>
     </div>
@@ -71,8 +126,11 @@ export default {
     data: () => ({
       fav: true,
       menu: false,
-      message: false,
-      hints: true,
+      checkbox: false,
+      admins: [
+          ['Management', 'people_outline'],
+          ['Settings', 'settings']
+      ]
     })
 }
 </script>
