9b23277dd8aeb83e0c22b4167eb6409bbfff0ab7
Gerard Mendiola <mendiolagerard@gmail.com>
2019-10-29 22:11:06 +0800

    commit

--- kpl_first_design/src/components/globalLayout/toolbar.vue
+++ kpl_first_design/src/components/globalLayout/toolbar.vue
@@ -1,31 +1,20 @@
 <template>
     <div class="green">
-        <v-toolbar class="green ml-12 mr-12 hidden-sm-and-down" flat height="50">
+        <v-toolbar class="green ml-12 mr-12 hidden-md-and-down" flat height="50">
             <v-toolbar-title class="white--text main_font hidden-sm-and-down">INSERT KPL LOGO</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items v-for="(item, index) in items" :key="index">
                 <v-btn text class="white--text">{{item.title}}</v-btn>
             </v-toolbar-items>
-
-
-            <v-toolbar-items class="hidden-lg-and-up">
-                <v-menu open-on-hover bottom offset-y>
-                    <template v-slot:activator="{ on }">
-                        <v-btn text class="green" dark v-on="on">
-                            Products
-                        </v-btn>
-                    </template>
-                </v-menu>
-            </v-toolbar-items>
         </v-toolbar>
-        <v-toolbar class="green hidden-md-and-up" flat height="50">
-            <v-app-bar-nav-icon color="white"  @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar class="green hidden-lg-and-up" flat height="50">
+            <v-app-bar-nav-icon color="white" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
             <v-spacer></v-spacer>
             <v-toolbar-title class="white--text main_font">INSERT KPL LOGO</v-toolbar-title>
         </v-toolbar>
 
-        <v-navigation-drawer v-model="drawer" class="hidden-md-and-up"
-            hide-overlay clipped app floating fixed>
+        <v-navigation-drawer v-model="drawer" class="hidden-lg-and-up"
+            hide-overlay clipped-left app floating fixed>
             <v-list dense>
                 <div v-for="(item, index) in items" :key="index">
                     <v-list-item @click="onSelectPage(item.link)"
