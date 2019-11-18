d8c879c41e62b781a471ce3d3a7f679b17875321
dev4 <gerardm@teknovarik.com>
2019-11-18 16:02:46 +0800

    commit

--- kpl_laravel/database/seeds/ItemSeeder.php
+++ kpl_laravel/database/seeds/ItemSeeder.php
@@ -266,49 +266,49 @@ class ItemSeeder extends Seeder
             [
                 'item_name' => 'Nike Zoom',
                 'item_description' => '35-45 sizes',
-                'category_id' => 4,
+                'category_id' => 5,
                 'brand_id' => 16
             ],
             [
                 'item_name' => 'KD 12',
                 'item_description' => '35-38 sizes',
-                'category_id' => 4,
+                'category_id' => 5,
                 'brand_id' => 16
             ],
             [
                 'item_name' => 'Lebron 13',
                 'item_description' => '40-49 sizes',
-                'category_id' => 4,
+                'category_id' => 5,
                 'brand_id' => 16
             ],
             [
                 'item_name' => 'Jordan 1',
                 'item_description' => 'red and white',
-                'category_id' => 4,
+                'category_id' => 5,
                 'brand_id' => 17
             ],
             [
                 'item_name' => 'Jordan Retro',
                 'item_description' => 'matte red',
-                'category_id' => 4,
+                'category_id' => 5,
                 'brand_id' => 17
             ],
             [
                 'item_name' => 'Van Gogh',
                 'item_description' => 'limited edition',
-                'category_id' => 4,
+                'category_id' => 5,
                 'brand_id' => 18
             ],
             [
                 'item_name' => 'old Skul',
                 'item_description' => 'triple black',
-                'category_id' => 4,
+                'category_id' => 5,
                 'brand_id' => 18
             ],
             [
                 'item_name' => 'High cut',
                 'item_description' => 'white version',
-                'category_id' => 4,
+                'category_id' => 5,
                 'brand_id' => 19
             ],
 
@@ -316,117 +316,117 @@ class ItemSeeder extends Seeder
             [
                 'item_name' => 'Happy Meal',
                 'item_description' => '150 only',
-                'category_id' => 5,
+                'category_id' => 6,
                 'brand_id' => 20
             ],
             [
                 'item_name' => 'Super Meal',
                 'item_description' => 'chicken, spaghetti, rice, and drinks',
-                'category_id' => 5,
+                'category_id' => 6,
                 'brand_id' => 20
             ],
             [
                 'item_name' => 'BFF Fries',
                 'item_description' => 'bbq flavor',
-                'category_id' => 5,
+                'category_id' => 6,
                 'brand_id' => 21
             ],
             [
                 'item_name' => 'Chicken Sandwich',
                 'item_description' => 'with fries',
-                'category_id' => 5,
+                'category_id' => 6,
                 'brand_id' => 21
             ],
             [
                 'item_name' => 'Coke Float',
                 'item_description' => 'large size',
-                'category_id' => 5,
+                'category_id' => 6,
                 'brand_id' => 21
             ],
             [
                 'item_name' => 'Flavored Shots',
                 'item_description' => 'spicy',
-                'category_id' => 5,
+                'category_id' => 6,
                 'brand_id' => 22
             ],
             // GADGETS
             [
                 'item_name' => 'Iphone XR',
                 'item_description' => '64gb',
-                'category_id' => 6,
+                'category_id' => 7,
                 'brand_id' => 23
             ],
             [
                 'item_name' => 'Airmac',
                 'item_description' => '256gb internal memory 16gb ram',
-                'category_id' => 6,
+                'category_id' => 7,
                 'brand_id' => 23
             ],
             [
                 'item_name' => 'S10+',
                 'item_description' => '32gb',
-                'category_id' => 6,
+                'category_id' => 7,
                 'brand_id' => 24
             ],
             [
                 'item_name' => 'Note 8+ edge',
                 'item_description' => '128gb internal memory',
-                'category_id' => 6,
+                'category_id' => 7,
                 'brand_id' => 24
             ],
             [
                 'item_name' => 'ROG',
                 'item_description' => '128gb ram',
-                'category_id' => 6,
+                'category_id' => 7,
                 'brand_id' => 25
             ],
             [
                 'item_name' => 'Switch',
                 'item_description' => 'all packages included',
-                'category_id' => 6,
+                'category_id' => 7,
                 'brand_id' => 26
             ],
             // CAFE
             [
                 'item_name' => 'Dark Mocha',
                 'item_description' => 'venti with sticker',
-                'category_id' => 7,
+                'category_id' => 8,
                 'brand_id' => 27
             ],
             [
                 'item_name' => 'Matcha',
                 'item_description' => 'grande with sticker',
-                'category_id' => 7,
+                'category_id' => 8,
                 'brand_id' => 27
             ],
             [
                 'item_name' => 'Coffee Jelly',
                 'item_description' => 'tall with fries',
-                'category_id' => 7,
+                'category_id' => 8,
                 'brand_id' => 28
             ],
             [
                 'item_name' => 'Cookies and Cream',
                 'item_description' => 'tall with burger',
-                'category_id' => 7,
+                'category_id' => 8,
                 'brand_id' => 28
             ],
             [
                 'item_name' => 'Choco Frappe',
                 'item_description' => 'venti including wifi password',
-                'category_id' => 7,
+                'category_id' => 8,
                 'brand_id' => 29
             ],
             [
                 'item_name' => 'Ciaometto Milk Tea',
                 'item_description' => 'large size',
-                'category_id' => 7,
+                'category_id' => 8,
                 'brand_id' => 30
             ],
             [
                 'item_name' => 'Meollo Hot Coffee',
                 'item_description' => 'sugar free',
-                'category_id' => 7,
+                'category_id' => 8,
                 'brand_id' => 30
             ]
         ];
