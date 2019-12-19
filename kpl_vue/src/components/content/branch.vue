da9c23fb66ddd6ace8c9256cd93add3f3ecdedc5
dev4 <gerardm@teknovarik.com>
2019-12-19 16:10:52 +0800

    commit changes

--- kpl_vue/src/components/content/branch.vue
+++ kpl_vue/src/components/content/branch.vue
@@ -1,15 +1,15 @@
 <template>
     <div v-resize="onResize">
         <v-card flat tile :class="$route.path == '/branch' ? 'animate_one' : ''"
-            :style="{ 'height': (windowSize.y-140)+'px' }">
+            :style="{ 'height': $route.path == '/branch' ? windowSize.y <= 736 ? '1010px' : '810px' : '' }">
             <v-card-title class="text-center py-12">
                 <v-spacer></v-spacer>
                 <span class="display-2 black--text">OUR BRANCH</span>
                 <v-spacer></v-spacer>
             </v-card-title>
 
-            <v-layout wrap>
-                <v-flex xs12 sm6 class="px-4 py-1">
+            <v-layout wrap class="pb-3">
+                <v-flex xs12 sm6 :class="windowSize.y <= 736 ? 'px-4 pb-4' : 'px-4'">
                     <v-card flat tile class="branch_one">
                         <GmapMap style="width: 100%; height: 350px;"
                             :center="{lat:14.6894942, lng:-238.972941}" :zoom="17" map-type-id="terrain"
@@ -18,17 +18,17 @@
                             </gmap-marker>
                         </GmapMap>
                         <v-layout wrap class="px-4 pt-2">
-                            <v-flex xs12 class="body-1 font-weight-bold">
+                            <v-flex xs12 class="headline font-weight-black">
                                 QUIRINO, QUEZON CITY
                             </v-flex>
-                            <v-flex xs12 class="body-2 font-weight-light">
-                                <v-icon small>local_library</v-icon>
+                            <v-flex xs12 class="subtitle-1 font-weight-medium grey--text text--darken-4">
+                                <v-icon small color="grey darken-4" class="mb-1">local_library</v-icon>
                                 536 Quirino Hwy, Novaliches, Quezon City, Metro Manila
                             </v-flex>
                         </v-layout>
                     </v-card>
                 </v-flex>
-                <v-flex xs12 sm6 class="px-4 py-1">
+                <v-flex xs12 sm6 class="px-4">
                     <v-card flat tile class="branch_two">
                         <GmapMap style="width: 100%; height: 350px;"
                             :center="{lat:14.7907806, lng:120.9322321}" :zoom="18" map-type-id="terrain"
@@ -37,11 +37,11 @@
                                 </gmap-marker>
                         </GmapMap>
                         <v-layout wrap class="px-4 pt-2">
-                            <v-flex xs12 class="body-1 font-weight-bold">
+                            <v-flex xs12 class="headline font-weight-black">
                                 MARILAO, BULACAN
                             </v-flex>
-                            <v-flex xs12 class="body-2 font-weight-light">
-                                <v-icon small>local_library</v-icon>
+                            <v-flex xs12 class="subtitle-1 font-weight-medium grey--text text--darken-4">
+                                <v-icon small color="grey darken-4" class="mb-1">local_library</v-icon>
                                 281 MacArthur Highway, Bocaue, 3018 Bulacan
                             </v-flex>
                         </v-layout>
