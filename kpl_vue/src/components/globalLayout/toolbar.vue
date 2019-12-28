9bc11682b39d0d920f832d113998aebbab64f30e
mardie <gonzales.mardie@gmail.com>
2019-12-28 13:35:12 +0800

    commit

--- kpl_vue/src/components/globalLayout/toolbar.vue
+++ kpl_vue/src/components/globalLayout/toolbar.vue
@@ -16,21 +16,21 @@
                             <span class="caption font-weight-bold white--text"
                                 style="cursor: default;">
                                 <v-icon style="font-size: 15px;" color="white" class="mr-1">phone</v-icon>
-                                <span class="font-weight-light white--text"> (+639) 286 3632</span>
+                                <span class="font-weight-light white--text"> (+632) 286 3632</span>
                             </span>
                         </v-flex>
                         <v-flex xs12 style="margin-top: -1%;">
                             <span class="caption font-weight-bold white--text"
                                 style="cursor: default;">
                                 <v-icon style="font-size: 15px;" color="white" class="mr-1">phone_android</v-icon>
-                                <span class="font-weight-light white--text"> +639 1597 52532</span>
+                                <span class="font-weight-light white--text"> (+639) 1597 52532</span>
                             </span>
                         </v-flex>
                         <v-flex xs12 style="margin-top: -1%;">
                             <span class="caption font-weight-bold white--text"
                                 style="cursor: default;">
                                 <v-icon style="font-size: 15px;" color="white" class="mr-1">print</v-icon>
-                                <span class="font-weight-light white--text"> +632 952 4265</span>
+                                <span class="font-weight-light white--text"> (+632) 952 4265</span>
                             </span>
                         </v-flex>
                         <v-flex xs12 style="margin-top: -1%;">
