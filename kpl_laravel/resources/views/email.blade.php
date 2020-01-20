a5d83d80e6527bac7ef720ba78944f515f8c4f03
mardie <mardieg@teknovarik.com>
2020-01-20 15:26:44 +0800

    commit

--- kpl_laravel/resources/views/email.blade.php
+++ kpl_laravel/resources/views/email.blade.php
@@ -7,9 +7,11 @@
     <br>
     <span style="font-size: 18px;"> Contact Number: {{$contact}}</span>
     <br>
-    <span style="font-size: 18px;"> Message: {{$mesage}}</span>
-    <br>
 
-    
-    
+    <span style="font-size: 18px;"> Inquired item:</span>
+    <ol>
+    @foreach ($checkbox as $item)
+        <li style="font-size: 18px;">{{$item['item_name']}}</li>
+    @endforeach
+    </ol>
 </div>
