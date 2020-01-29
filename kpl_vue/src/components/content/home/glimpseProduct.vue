e9e7e4968b424d25e1aa0faf7589c68602790d87
dev4 <gerardm@teknovarik.com>
2020-01-29 14:07:59 +0800

    commit changes

--- kpl_vue/src/components/content/home/glimpseProduct.vue
+++ kpl_vue/src/components/content/home/glimpseProduct.vue
@@ -9,14 +9,19 @@
                         <span class="caption font-weight-medium grey--text text--darken-2 ml-2">Products</span>
                     </v-list-item-title>
                 </v-list-item>
-                <div v-for="(item, index) in categoryList" :key="index">
-                    <v-list-item @click="onFilterByCategory(item)"
-                        style="border-bottom: 1px solid #E0E0E0;"
-                        :class="item.id == category_id ? 'grey lighten-2' : ''">
-                        <v-list-item-title class="caption">
-                            {{item.category_name}}
-                        </v-list-item-title>
-                    </v-list-item>
+                <div :style="{
+                    'height': $route.path != '/product' ? '315px' : '',
+                    'overflow-y': $route.path != '/product' ? 'auto' : '',
+                    'overflow-x': $route.path != '/product' ? 'hidden' : '' }">
+                    <div v-for="(item, index) in categoryList" :key="index">
+                        <v-list-item @click="onFilterByCategory(item)"
+                            style="border-bottom: 1px solid #E0E0E0;"
+                            :class="item.id == category_id ? 'grey lighten-2' : ''">
+                            <v-list-item-title class="caption">
+                                {{item.category_name}}
+                            </v-list-item-title>
+                        </v-list-item>
+                    </div>
                 </div>
             </v-list>
         </v-card>
